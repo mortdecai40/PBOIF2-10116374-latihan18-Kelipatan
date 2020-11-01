f285d4014c5f7aae4dee8b1316bf6bc0ef32e5d6
mortdecai40 <rizkinabil.999@gmail.com>
2020-11-01 09:59:50 +0700

    latihan 18

---
+++ src/pbo02/pkg10116374/latihan18/kelipatan/PBO0210116374Latihan18Kelipatan.java
@@ -0,0 +1,23 @@
+package pbo02.pkg10116374.latihan18.kelipatan;
+
+/** @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk menghitung kelipatan
+ *
+ */
+public class PBO0210116374Latihan18Kelipatan {
+
+     public static void kelipatan(){
+        for (double i = 3.5; i <= 35;) {
+            System.out.println(i);
+            i += 3.5;
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        kelipatan();
+    }
+    
+}
